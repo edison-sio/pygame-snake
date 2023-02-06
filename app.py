3ff21270719a805ff2d869d90a7d0861d5be6148
Edison Sio <65646946+Addidson@users.noreply.github.com>
2023-02-07 02:46:42 +1100

    playable

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 import pygame as pg
 import os
 
-from game import Game
+from game import Game, LEFT, RIGHT, UP, DOWN
 
 # WIN = pg.display.set_mode((500, 500))
 clock = pg.time.Clock()
 
 class SnakeGameWindow:
     def __init__(self, title: str='Snake Game', 
-                 width: int=900, 
+                 width: int=500, 
                  height: int=500, 
                  fps: int=60,
-                 theme: str='') -> None:
+                 theme: str='default') -> None:
         '''
         Initiate snake game window
         '''
         # Windows init
         self.win = pg.display.set_mode((width, height))
         self.fps = fps
-        self.game = Game()
+        self.game = Game(23, 23)
+        self.game.start_game()
+
+        self.frame_count = 0
+
+        self.block_side = 20
         
         # Game init
         # self.background = pg.image.load(os.path.join('Assets', {theme}, 'background.png'))
-        self.background = (255, 255, 255)
-        self.head = pg.transform.scale(pg.image.load(os.path.join('Assets', 'head.png')), (100, 100))
-        # self.body = pg.image.load(os.path.join('Assets', {theme}, 'body.png'))
-        # self.food = pg.image.load(os.path.join('Assets', {theme}, 'food.png'))
-        # self.empty = pg.image.load(os.path.join('Assets', {theme}, 'empty.png'))
+        self.background = (25, 25, 25)
+        self.head = pg.transform.scale(pg.image.load(os.path.join('assets', theme, 'body.png')), (self.block_side, self.block_side))
+        self.body = pg.transform.scale(pg.image.load(os.path.join('Assets', theme, 'body.png')), (self.block_side, self.block_side))
+        self.food = pg.transform.scale(pg.image.load(os.path.join('Assets', theme, 'food.png')), (self.block_side, self.block_side))
+        self.empty = pg.transform.scale(pg.image.load(os.path.join('Assets', theme, 'empty.png')), (self.block_side, self.block_side))
         
-
-
         pg.display.set_caption(title)
 
     def mainloop(self):
@@ -42,16 +45,47 @@ class SnakeGameWindow:
         pg.quit()
     
     def _update(self):
-        self._draw_window()
         clock.tick(self.fps)
+        
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_LEFT]:
+            self.game.change_direction(LEFT)
+        if keys_pressed[pg.K_RIGHT]:
+            self.game.change_direction(RIGHT)
+        if keys_pressed[pg.K_UP]:
+            self.game.change_direction(UP)
+        if keys_pressed[pg.K_DOWN]:
+            self.game.change_direction(DOWN)
+        
+        if self.frame_count == self.block_side: 
+            if self.game.update():
+                self.fps += 1
+            self.frame_count = 0
+        self.frame_count += 1
+        self._draw_window()
     
     def _draw_window(self):
         self.win.fill(self.background)
-        # self.win.blit(self.head, (0, 0))
-        for y in range(5):
-            for x in range(9):
-                # print(row, col)
-                self.win.blit(self.head, (x*100, y*100))
+        empty_positions = self.game.empty
+        snake_positions = self.game.snake
+        food_position = self.game.food
+
+        # Draw empty
+        for pos in empty_positions:
+            self.win.blit(self.empty, ((pos.x+1) * self.block_side, (pos.y+1) * self.block_side))
+        
+        # Draw snake
+        is_head = True
+        for pos in snake_positions:
+            if is_head:
+                self.win.blit(self.head, ((pos.x+1) * self.block_side, (pos.y+1) * self.block_side))
+                is_head = False
+            else:
+                self.win.blit(self.body, ((pos.x+1) * self.block_side, (pos.y+1) * self.block_side))
+        
+        # Draw food
+        self.win.blit(self.food, ((food_position.x+1) * self.block_side, (food_position.y+1) * self.block_side))
+                
         pg.display.update()
 
 if __name__ == '__main__':
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from random import randint
 
 class GridValue(Enum):
     EMPTY = 0
@@ -10,14 +11,25 @@ class Pair:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __add__(self, other):
+        return Pair(self.x + other.x, self.y + other.y)
 
-class Direction:
+class Direction(Pair):
     def __init__(self, x, y):
-        Pair(x, y)
+        super().__init__(x, y)
 
-class Position:
+LEFT = Direction(-1, 0)
+RIGHT = Direction(1, 0)
+UP = Direction(0, -1)
+DOWN = Direction(0, 1)
+
+class Position(Pair):
     def __init__(self, x, y):
-        Pair(x, y)
+        super().__init__(x, y)
     
     def next_position(self, dir: Direction):
         return Position(self.x + dir.x, self.y + dir.y)
@@ -26,10 +38,61 @@ class Position:
 class Game:
     def __init__(self, width, height):
         self.grid = [[GridValue.EMPTY for _ in range(width)] for _ in range(height)]
+        self.width = width
+        self.height = height
+        self.empty = []
         self.food = None
         self.snake = []
+        self.game_over = True
     
     def start_game(self):
         for i in range(3):
-            self.snake.append(Position(i, 0))
+            self.snake.append(Position(2-i, 0))
+        self.direction = RIGHT
+        self.update_empty()
+        # self.update_food()
+        self.food = Position(14, 0)
+        self.game_over = False
+    
+    def change_direction(self, direction):
+        if self.direction + direction == Direction(0, 0):
+            return
+        self.direction = direction
+
+    def update(self):    
+        head = self.snake[0]
+        next_pos = head.next_position(self.direction)
+        print(next_pos.x, next_pos.y)
+        if next_pos.x >= self.width or next_pos.x < 0 or next_pos.y >= self.height or next_pos.y < 0:
+            self.game_over = True
+            return
+        if next_pos in self.snake[:-1]:
+
+            self.game_over = True
+            return
+        self.snake.insert(0, next_pos)
+        if next_pos != self.food:
+            self.snake.remove(self.snake[-1])
+            self.update_empty()
+            return True
+        else:
+            self.update_empty()
+            self.update_food()
+        
+    def update_empty(self):
+        self.empty.clear()
+        for y in range(self.height):
+            for x in range(self.width):
+                pos = Position(x, y)
+                if pos not in self.snake:
+                    self.empty.append(pos)
+    
+    def update_food(self):
+        rand_i = randint(0, len(self.empty)-1)
+        self.food = self.empty[rand_i]
+        self.empty.remove(self.food)
+    
+    # def _get_empty(self):
+    #     for y in self.grid:
+    #         for x in 
         
